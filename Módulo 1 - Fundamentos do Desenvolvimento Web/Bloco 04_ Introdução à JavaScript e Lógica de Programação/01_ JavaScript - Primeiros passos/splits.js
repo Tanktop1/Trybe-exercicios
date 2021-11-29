29ce41850282acb8708e5a69bc4ae863a9202c12
Gideão de Carvalho <gideaox78@gmail.com>
2021-11-29 09:54:14 -0300

    adiciona variáveis

--- Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04_ Introdução à JavaScript e Lógica de Programação/01_ JavaScript - Primeiros passos/splits.js
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04_ Introdução à JavaScript e Lógica de Programação/01_ JavaScript - Primeiros passos/splits.js
@@ -1,5 +1,11 @@
-let age = 24;
+const age = 24;
 let nome = "Gideão";
+const myName = "Gideão de Carvalho";
+const birthCity = "Pindamonhangaba";
+let birthYear = 2030;
 
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
 console.log(age);
 console.log(nome);
