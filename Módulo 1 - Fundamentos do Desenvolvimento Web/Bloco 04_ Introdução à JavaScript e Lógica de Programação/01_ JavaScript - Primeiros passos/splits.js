17bd1d15ea13d9479719d384fc0c8cdd62a95ed7
Gideão de Carvalho <gideaox78@gmail.com>
2021-11-29 10:24:50 -0300

    Tipos Primitivos

--- Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04_ Introdução à JavaScript e Lógica de Programação/01_ JavaScript - Primeiros passos/splits.js
+++ Módulo 1 - Fundamentos do Desenvolvimento Web/Bloco 04_ Introdução à JavaScript e Lógica de Programação/01_ JavaScript - Primeiros passos/splits.js
@@ -1,3 +1,6 @@
+//CONSTANTES E VARIÁVEIS
+
+
 const age = 24;
 let nome = "Gideão";
 const myName = "Gideão de Carvalho";
@@ -9,3 +12,23 @@ console.log(birthCity);
 console.log(birthYear);
 console.log(age);
 console.log(nome);
+
+// TIPOS PRIMITIVOS, TIPAGEM DINÂMICA E OPERAÇÕES ARITMÉTICAS
+
+let movie = "Avengers"; //string litral
+let score = 10; //number literal
+let isValid = true; //boolean
+let name; //undefined
+let color = null; //redefinição
+let salary = 3950;
+
+console.log(salary + salary);
+salary++;
+console.log(salary);
+
+  const base = 5;
+  let height = 8;
+  const area = base * height;
+  const perimeter = base + height + area;
+  console.log(area);
+  console.log(perimeter);
